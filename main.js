5ee083b8ba42bbb9cef78d9e59054b69405a6415
elrompecucas123 <Dasp20011@outlook.com>
2020-09-08 23:32:22 -0500

    new new

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function Mostrar(){
-    document.getElementById('SubMenu').style.display="none";
+    // document.getElementById('SubMenu').style.display="none";
+    var items = document.getElementsByClassName('item');
+    console.log(items);
+    for (let index = 0; index < items.length; index++) {
+        items[index].style.display = "block";
+    }
 }
 function Ocultar(){
-    document.getElementById('SubMenu').style.display="block";
+    var items = document.getElementsByClassName('item');
+    console.log(items);
+    for (let index = 0; index < items.length; index++) {
+        items[index].style.display = "none";
+    }
 }
